785d05b1b014309816d48a008b09cc239339041e
guanwenxin <764434043@qq.com>
2022-11-22 19:28:16 +0800

    流程控制

--- src/js/operator.js
+++ src/js/operator.js
@@ -17,8 +17,8 @@ console.log(b)
 
 
 //逻辑运算
-//1.&&与
-//2.||非
+//1.&&与   只有两个位都为1的时候1
+//2.||非    只有两个位都为0的时候0，只要有一个1就是1
 const bb = undefined;
 const aaa = bb || 5;
 console.log('a',aaa)
@@ -34,7 +34,8 @@ else{
     console.log('没进来')
 }
 
-// 只用===，包含了所有的类型
+// == 有类型转化
+// 在js中，只用===，包含了所有的类型，没有类型转化
 // !=,==,!==,===
 if (true !== 1) {
     console.log('aaa')
@@ -50,7 +51,7 @@ const people = {
     }
 }
 // optional chainning可选链运算符
-console.log(people?.family?.sister?.name)
+console.log(people?.family?.sister?.name)   //取不到就会返回undefined
 
 
 
@@ -68,29 +69,32 @@ for(let i = 0; i < 5; i++) {
 
 while(false) {
 
-}
+}                 // 死循环
 
 do {
 
-} while(false)
+} while(false)    //先执行一次
 
 
 const nameLong = 'hahaha'
 switch(nameLong) {
+    //case可以直接匹配字符串
     case 'hahaha': {
         console.log('匹配上了')
         break;
     }
 
     default: {
-        console.log('不知道啥名')
+        console.log('不知道啥名')      //没有匹配的上就是走default
     }
 }
 
 
 
 // 经典函数
+// js里面一切都可以视为变量，函数也快呀作为变量 
 // 没有函数重载  函数名不可重复 
+// 括号里面可以声明形参
 function test () {
     const a = 1;
     //返回值
@@ -105,3 +109,41 @@ function add (name,age){
 const name = 'aaa'
 const age = 111
 add(name,age)
+
+//调用和声明的区分
+//运行的时候，后面一定会没有有大括号{}，没有的话一定是执行调用
+//有大括号一定是在声明
+
+// 简写
+const functionSet = {
+    //没有简写前  test： function test（）
+    test() {
+    },
+    add,     //key value 都是add，但是value是一个函数
+}
+
+
+const renameADD = add;
+// 调用的时候加一个括号就行
+// renameADD(1, 2)
+console.log('加法测试', renameADD(1, 2))
+
+
+// 匿名函数
+// 没有函数名，声明函数的时候直接以变量的方式
+const nimingfunction = function() {
+    console.log('nimingfunction')
+}
+
+
+// 箭头函数 = 整个函数的返回值 + 参数
+const arrowFunction = () => console.log('arrowFunction');
+// a+b默认作为返回值返回了
+const arrowFunctionParam = (a, b) => a + b;
+// 复杂
+const arrowFunctionParam2 = (a, b) => {
+    const t = a;
+    const s = b;
+    return t + s;
+}
+console.log(arrowFunctionParam2(1, 2))
