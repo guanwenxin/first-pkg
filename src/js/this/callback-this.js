ab8e2435f5d719ddf23669dd96633692a47f9705
guanwenxin <764434043@qq.com>
2022-11-30 14:22:09 +0800

    回调函数场景

--- src/js/this/callback-this.js
+++ src/js/this/callback-this.js
@@ -1,16 +1,28 @@
 // 回调函数场景
+// 箭头函数没有this
+// 回调函数尽量使用箭头函数
 const paramObj = {
-    name: 'pjw',
+    name: 'hahahha',
     getName() {
         console.log(this)
     },
-    //1.
+
+    //最原始  undefined
+    timer0: function () {
+        setTimeout(function () {
+            //this.name是setTimeout内部的对象
+            console.log(this.name, '1s')   //此处的this.name指向的不是5行的name: 'hahahha',
+        }, 1000)
+    },
+
+    //1. bind绑定
     timer: function () {
         setTimeout(function () {
-            console.log(this.name, '1s')
-        }.bind(this), 1000)
+            console.log(this.name, '1s')   
+        }.bind(this), 1000)      //自己写一个绑定函数绑定
     },
-    //2.
+
+    //2.回调函数 外部环境的this是什么，内部就是什么
     timer2: function () {
         setTimeout(() => {
             console.log(this.name, '1s')
@@ -18,9 +30,17 @@ const paramObj = {
     }
 }
 
-paramObj.timer()
+//undefined
+paramObj.timer0()
+//hahahha 1s
+paramObj.timer()   
+paramObj.timer2()
+
+
 
 const test = paramObj.getName
+
+
 
 
 
